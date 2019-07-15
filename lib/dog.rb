b8d8cfa33eaea18bca2a9a0be29af1b997e10c51
Trish Ogen <patriciaogen@gmail.com>
2019-07-15 00:40:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
 
   attr_accessor :id, :name, :breed
 
-  def initilaize(name:, breed:)
-    @id = nil
+  def initilaize(name:, breed:, id: nil)
+    @id = id
     @name = name
     @breed = breed
   end
