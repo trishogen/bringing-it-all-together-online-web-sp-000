a3825fe270a7ed7920f4411d81c906ee02df0f2f
Trish Ogen <patriciaogen@gmail.com>
2019-07-15 01:16:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
   def self.find_by_name(name:)
     sql = "select * from dogs where name = ?"
     row = DB[:conn].execute(sql, name)[0]
-    self.new_from_db(row)
+    Dog.new_from_db(row)
   end
 
 end
