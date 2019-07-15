7ff199315255e7d3b9adfa41e30285f164427e11
Trish Ogen <patriciaogen@gmail.com>
2019-07-15 00:47:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,15 @@ class Dog
   end
 
   def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+
     
+    Dog.new
   end
 
 end
